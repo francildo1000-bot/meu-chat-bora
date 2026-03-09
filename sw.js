bf6c05235d40eeed416568b0f48d001d572055f2
Admin-Hells~ <francildo1000@gmail.com>
2026-03-09 01:13:57 -0300

    mudança de projeto

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'distrito-zero-v1';
+const assets = ['/', 'index.html', 'style.css', 'script.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
